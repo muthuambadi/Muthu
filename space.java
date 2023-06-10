3934f0060b3ff28f832602caf88cbe2ecf502e44
muthuambadi <132544818+muthuambadi@users.noreply.github.com>
2023-06-10 08:50:58 +0530

    Add files via upload

---
+++ space.java
@@ -0,0 +1,22 @@
+public class space
+{
+public static void main(String[] args)
+{
+
+String s="malala got nobel prize for peace";
+
+int count=0;
+
+for(int i=0;i<s.length();i++)
+{
+if(s.charAt(i)==' ')
+{
+count++;
+}
+}
+System.out.println(count);
+System.out.println(count+1);
+
+}
+}
+
